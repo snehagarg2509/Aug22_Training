11e8d68174ec6e0ce02c501aa8ebce1001454975
snehagarg2509 <user35@trainux01.jrf11vejvqfuhbwnm0ewggipoa.ax.internal.cloudapp.net>
2024-09-02 11:06:16 +0000

    First program

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+/* This is a hello world program.
+Owner : Sneha Date: 02-09-24
+Revision History
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("Hello,\nMy name is Sneha Garg.\n");
+	printf("Add: P-67/1, Raksha Vihar Colony,\nCity Centre, Gwalior\n");
+	printf("M.P. [474002]\n");
+
+	return EXIT_SUCCESS;
+}
+
+
